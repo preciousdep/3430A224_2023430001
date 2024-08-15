06ef4e0665dccdeaea551cfe4bc202fd0c12368d
preciousdep <aosthetic@gmail.com>
2024-08-15 12:11:04 -0400

    mover archivos a carpeta

---
+++ laboratorio_0/calculocuadrado.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+double square(double x)
+{
+    return x*x;
+}
+
+void print_square(double x)
+{
+    std::cout <<"The square of " <<x << " is " << square(x)<< '\n';
+}
+
+int main()
+{
+    print_square(5);
+    print_square(6);
+}
---
+++ laboratorio_0/laboratorio_0.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int numero()
+{
+    std::cout << "Ingrese un numero\n";
+    int x;
+    // leer desde estandar
+    std::cin >> x;
+    // imprimir con estandar
+    std::cout << x <<"\n";
+    return x;
+}
+
+int par_o_no(int x)
+// determinar si es par o no
+{
+    if ( x % 2 == 0) {
+        std::cout << x << " es un numero par\n";
+    } else {
+        std::cout << x << " no es un numero par\n";
+    }
+
+}
+
+int cuenta_string()
+{
+    std::string texto;
+    int contador = 0;
+    std::cout << "Ingrese un texto para contar su largo\n";
+    std::cin.ignore();
+    std::getline(std::cin, texto);
+    for (auto i=0; i< texto.length(); ++i) {
+        contador ++;
+    };
+    std::cout << "En el texto ingresado hay " << contador << " caracteres (incluye espacios)\n";
+}
+
+int main()
+{
+    int numerouser = numero();
+    par_o_no(numerouser);
+    for (int i = 1; i <= 10; i++) {
+        par_o_no(i);
+    };
+    cuenta_string();
+
+}
