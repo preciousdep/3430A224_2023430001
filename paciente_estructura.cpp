07723c8bc8d8cac62e396869d7d86c73455ec03c
preciousdep <aosthetic@gmail.com>
2024-08-27 09:08:55 -0400

    prueba branch

---
+++ paciente_estructura.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+// struct for patient with name and age
+struct Patient {
+    std::string name;
+    int age;
+    // in kg
+    int weight;
+    // in cm
+    int height;
+};
+
+int main() {
+// creating patients' info
+    
+}
