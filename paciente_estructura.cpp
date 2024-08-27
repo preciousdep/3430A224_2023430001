7b7bbb53e96f05fa92ca6146babdc12c53db67f3
preciousdep <aosthetic@gmail.com>
2024-08-27 09:28:17 -0400

    estructura

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 // struct for patient with name and age
 struct Patient {
@@ -11,7 +12,20 @@ struct Patient {
     int height;
 };
 
+void print_info(Patient patient) {
+    cout << "Patient \n Name: " 
+    << patient.name << "\n Age: " 
+    << patient.age << "\n Weight in kg: " 
+    << patient.weight << "\n Height in cm: "
+    << patient.height;
+};
+
 int main() {
 // creating patients' info
-    
+    Patient pepe = {"Pepe", 20, 70, 170};
+    print_info(pepe);
+    Patient lisa = {"Lisa", 24, 62, 150};
+    print_info(lisa);
+    Patient live = {"Live", 30, 65, 180};
+    print_info(live);
 }
